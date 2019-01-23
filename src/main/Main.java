30092094b8905bb0e8180de385fae307cca5f887
Yshika <yshika0581@gmail.com>
2019-01-23 15:02:31 +0530

    add array

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,23 @@
 package main;
 
-public class Main {
+
+public class NumbersArray {
+    int sum=0;
+    int[] numbers=new int[10];
+    boolean checksum()
+    {
+        boolean response=false;
+        for(int i=0;i<10;i++)
+        {
+            sum+=numbers;
+
+        }
+        if (sum>0){
+            response=true;
+        }
+        return response;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello");
+
     }
 }
